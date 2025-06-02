160c9f697714ae0ceaf1788e71ed41999b4888cc
龚宜炯 <gongyijiong@eyenice.cn>
2025-06-02 10:28:00 +0800

    fix: 优化WiFi重连处理

--- components/blufi/blufi.c
+++ components/blufi/blufi.c
@@ -502,3 +502,103 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t e
                         free(ap_list);
                     }
                     else {
+                        BLUFI_ERROR("Failed to allocate memory for reconnect scan results\n");
+                    }
+                }
+                else {
+                    BLUFI_INFO("No AP found in reconnect scan\n");
+                    wifi_scan_attempt_count++;
+                }
+                break;
+            }
+
+            /* 原有的BluFi扫描逻辑 */
+            if (apCount == 0) {
+                BLUFI_INFO("Nothing AP found");
+                break;
+            }
+            wifi_ap_record_t* ap_list =
+                (wifi_ap_record_t*)malloc(sizeof(wifi_ap_record_t) * apCount);
+            if (!ap_list) {
+                BLUFI_ERROR("malloc error, ap_list is NULL");
+                esp_wifi_clear_ap_list();
+                break;
+            }
+            ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&apCount, ap_list));
+            esp_blufi_ap_record_t* blufi_ap_list =
+                (esp_blufi_ap_record_t*)malloc(apCount * sizeof(esp_blufi_ap_record_t));
+            if (!blufi_ap_list) {
+                if (ap_list) {
+                    free(ap_list);
+                }
+                BLUFI_ERROR("malloc error, blufi_ap_list is NULL");
+                break;
+            }
+            for (int i = 0; i < apCount; ++i) {
+                blufi_ap_list[i].rssi = ap_list[i].rssi;
+                memcpy(blufi_ap_list[i].ssid, ap_list[i].ssid, sizeof(ap_list[i].ssid));
+            }
+
+            if (ble_is_connected == true) {
+                esp_blufi_send_wifi_list(apCount, blufi_ap_list);
+            }
+            else {
+                BLUFI_INFO("BLUFI BLE is not connected yet\n");
+            }
+
+            esp_wifi_scan_stop();
+            free(ap_list);
+            free(blufi_ap_list);
+            break;
+        }
+        case WIFI_EVENT_AP_STACONNECTED: {
+            wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*)event_data;
+            BLUFI_INFO("station " MACSTR " join, AID=%d", MAC2STR(event->mac), event->aid);
+            break;
+        }
+        case WIFI_EVENT_AP_STADISCONNECTED: {
+            wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*)event_data;
+            BLUFI_INFO("station " MACSTR " leave, AID=%d, reason=%d", MAC2STR(event->mac),
+                       event->aid, event->reason);
+            break;
+        }
+
+        default:
+            break;
+    }
+    return;
+}
+
+static void initialise_wifi(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    wifi_event_group = xEventGroupCreate();
+    esp_netif_t* sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+    esp_netif_t* ap_netif = esp_netif_create_default_wifi_ap();
+    assert(ap_netif);
+    ESP_ERROR_CHECK(
+        esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(
+        esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL));
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    example_record_wifi_conn_info(EXAMPLE_INVALID_RSSI, EXAMPLE_INVALID_REASON);
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+static esp_blufi_callbacks_t example_callbacks = {
+    .event_cb = example_event_callback,
+    .negotiate_data_handler = blufi_dh_negotiate_data_handler,
+    .encrypt_func = blufi_aes_encrypt,
+    .decrypt_func = blufi_aes_decrypt,
+    .checksum_func = blufi_crc_checksum,
+};
+
+static void example_event_callback(esp_blufi_cb_event_t event, esp_blufi_cb_param_t* param)
+{
+    /* actually, should post to blufi_task handle the procedure,
+     * now, as a example, we do it more simply */
+    switch (event) {
